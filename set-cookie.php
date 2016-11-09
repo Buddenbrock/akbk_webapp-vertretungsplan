cc9f2ad592bfa2d170d2a36a22b4a2ecbf849c35
Buddenbrock <dbuddenbrock@t-online.de>
2016-11-09 19:24:05 +0100

    Erweiterung der Einstellungsfunktionen

--- set-cookie.php
+++ set-cookie.php
@@ -8,11 +8,14 @@
   $color_id = $_REQUEST['color_id'];
 
   // Cookies werden 180 Tage gespeichert
-  $save_time = time()+(3600*4320);
+  $save_time_long = time()+(3600*4320);
 
-  setcookie("show-class-id", $class_id, $save_time);
-  setcookie("show-class-name", $class_name, $save_time);
-  setcookie("color-id", $color_id, $save_time);
+  // Cookies werden 20 Tage gespeichert
+  $save_time_short = time()+(3600*480);
+
+  setcookie("show-class-id", $class_id, $save_time_short);
+  setcookie("show-class-name", $class_name, $save_time_short);
+  setcookie("color-id", $color_id, $save_time_long);
 
   header('Location: index.php');
 ?>
