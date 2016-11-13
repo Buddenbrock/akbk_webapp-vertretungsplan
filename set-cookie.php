691e6578952f331f3371bf19ee7305c12a6d0e34
Buddenbrock <dbuddenbrock@t-online.de>
2016-11-13 15:27:58 +0100

    Umbau der set-cookie für individuelle Speicherung

--- set-cookie.php
+++ set-cookie.php
@@ -3,19 +3,38 @@
 
   include_once('assets/includes/config.php');
 
-  $class_id = $_REQUEST['class_id'];
-  $class_name = $class_name[($class_id - 1)];
-  $color_id = $_REQUEST['color_id'];
-
   // Cookies werden 180 Tage gespeichert
   $save_time_long = time()+(3600*4320);
 
   // Cookies werden 20 Tage gespeichert
   $save_time_short = time()+(3600*480);
 
-  setcookie("show-class-id", $class_id, $save_time_short);
-  setcookie("show-class-name", $class_name, $save_time_short);
-  setcookie("color-id", $color_id, $save_time_long);
+
+  if(isset($_REQUEST['class_id'])){
+    $class_id = $_REQUEST['class_id'];
+    $class_name = $class_name[($class_id - 1)];
+
+    setcookie("show-class-id", $class_id, $save_time_short);
+    setcookie("show-class-name", $class_name, $save_time_short);
+  }
+
+  if(isset($_REQUEST['color_id'])){
+    $color_id = $_REQUEST['color_id'];
+
+    setcookie("color-id", $color_id, $save_time_long);
+  }
+
+  if(isset($_REQUEST['button_1'] && $_REQUEST['button_2'] && $_REQUEST['button_3'] && $_REQUEST['button_4'])){
+    $button_1 = $_REQUEST['button_1'];
+    $button_2 = $_REQUEST['button_2'];
+    $button_3 = $_REQUEST['button_3'];
+    $button_4 = $_REQUEST['button_4'];
+
+    setcookie("button_1", $button_1, $save_time_long);
+    setcookie("button_2", $button_2, $save_time_long);
+    setcookie("button_3", $button_3, $save_time_long);
+    setcookie("button_4", $button_4, $save_time_long);
+  }
 
   header('Location: index.php');
 ?>
